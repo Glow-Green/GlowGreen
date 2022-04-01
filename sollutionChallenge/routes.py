a441c1c833b4c394fc6892175cbd03b5685fd065
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2022-04-02 00:16:57 +0530

    Update routes.py

--- sollutionChallenge/routes.py
+++ sollutionChallenge/routes.py
@@ -6,10 +6,6 @@ from sollutionChallenge import app
 from sollutionChallenge.utils import access_camera
 import numpy as np
 
-DETECTION_THRESHOLD = 0.1
-options = ObjectDetectorOptions(num_threads=4,
-                                score_threshold=DETECTION_THRESHOLD)
-detector = ObjectDetector(model_path="sollutionChallenge/assets/apple_lr_1.0.0_beta.tflite", options=options)
 
 @app.route('/')
 @app.route('/home')
